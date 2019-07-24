90c0bce859de8c6bc1336007ab11e9dfb79db2c6
elizeudesantana <elizeudesantana@hotmail.com>
2019-07-24 18:08:17 -0300

    Healthcheck

--- source/healthcheck/views.py
+++ source/healthcheck/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+from django.contrib.auth.models import User
+import logging
 
-# Create your views here.
+log = logging.getLogger(__name__)
+
+class HealthcheckAPIView(APIView):
+
+    def get(self, request, format=None):
+        database_healthy = True
+        cursor = none
+        try:
+            cursor = connection.cursor()
+            cursor.execute('SELECT 1;')
+        except Exception as Err:
+            database_healthy = False
+            log.exception(err)
+        finally:
+            if cursor is not None:
+                cursor.close()
+        data={
+            'database'= database_healthy
+        }
+        return Response(data)
